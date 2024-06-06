ab7f05ea7f469488a0119b0044a686527b120ea2
Vitor Nery <vitormanoel2020@gmail.com>
2024-06-06 02:44:00 -0300

    Adicionada a página de cursos e atualização no init-swiper

--- JS/init-swipers.js
+++ JS/init-swipers.js
@@ -1,25 +1,25 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-    
-const swiper = new Swiper('.mostAcessed-swiper', {
-  direction: 'horizontal',
-  slidesPerView: 3  ,
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
+const swiper = new Swiper(".mostAcessed-swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
   loop: true,
-  grabCursor:true,
+  grabCursor: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  breakpoints:{
+  breakpoints: {
     0: {
       slidesPerView: 1,
-      allowSlideNext:'true',
-      allowSlidePrev:'true',
+      allowSlideNext: "true",
+      allowSlidePrev: "true",
     },
     576: {
       slidesPerView: 2,
     },
-    900:{
+    900: {
       slidesPerView: 3,
     },
     1024: {
@@ -28,65 +28,82 @@ const swiper = new Swiper('.mostAcessed-swiper', {
     1300: {
       slidesPerView: 5,
     },
-  }
+  },
 });
 
-      const swiper2 = new Swiper('.recently-swiper', {
-        direction: 'horizontal',
-        slidesPerView: 3  ,
-        loop: true,
-        grabCursor:true,
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        breakpoints:{
-          0: {
-              slidesPerView: 1,
-              allowSlideNext:'true',
-              allowSlidePrev:'true',
-          },
-          576: {
-              slidesPerView: 2,
-              
-          },
-          900:{
-              slidesPerView: 3,
-          },
-          1024: {
-            slidesPerView: 4,
-          },
-          1300: {
-            slidesPerView: 5,
-          },
-        }
-      });
+const swiper2 = new Swiper(".recently-swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  loop: true,
+  grabCursor: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      allowSlideNext: "true",
+      allowSlidePrev: "true",
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1300: {
+      slidesPerView: 5,
+    },
+  },
+});
 
-    const swiper3 = new Swiper('.feedback-swiper', {
-        direction: 'horizontal',
-        slidesPerView: 3  ,
-        loop: true,
-        grabCursor:true,
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        breakpoints:{
-          0: {
-              slidesPerView: 1,
-              allowSlideNext:'true',
-              allowSlidePrev:'true',
-          },
-          576: {
-              slidesPerView: 2,
-              
-          },
-          900:{
-              slidesPerView: 3,
-          },
-          
-        }
-      });
-      
+const swiper3 = new Swiper(".feedback-swiper", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  loop: true,
+  grabCursor: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      allowSlideNext: "true",
+      allowSlidePrev: "true",
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+  },
+});
+const swiper4 = new Swiper(".swiper-course", {
+  direction: "horizontal",
+  slidesPerView: 1,
+  loop: false,
+  allowSlideNext: "true",
+  allowSlidePrev: "true",
+  grabCursor: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      allowSlideNext: "true",
+      allowSlidePrev: "true",
+    },
+    576: {},
+    900: {},
+  },
+});
